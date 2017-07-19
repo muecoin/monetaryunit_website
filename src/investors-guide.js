788447c83cc7d0fff16b2987e6ee10c04c8cbf59
Mark Kneale <mark.kneale@antiblanks.com>
2017-07-19 22:15:13 +0100

    fixed html encoding issues

--- src/investors-guide.js
+++ src/investors-guide.js
@@ -32,7 +32,7 @@ export default () => {
         <GuideStep
           step="1"
           title="Download your wallet"
-          tipOne="The first time you load your wallet, it may take a while to sync. Don’t worry, it only happens once!"
+          tipOne="The first time you load your wallet, it may take a while to sync. Don't worry, it only happens once!"
           tipTwo="Your could also use our secure online wallet webwallets.io"
           imageUrl="assets/images/guides/investors/buying-mue/step-1.png">
             <p className="guide-step__paragraph">Wallets store your digital currencies. Just install them on your preferred device and wait for them to sync up with the network. You could also choose to use one of our secure online at <a className="guide-intro__paragraph-link" href="http://www.webwallets.io/" target="_blank">webwallets.io</a></p>
@@ -44,12 +44,12 @@ export default () => {
           step="2"
           title="Get verified on Bittylicious"
           tipOne="You can still make anonymous purchases but the amount you can buy is severely reduced without verification."
-          tipTwo="Once you’ve submitted your documents, you should hear back shortly."
+          tipTwo="Once you've submitted your documents, you should hear back shortly."
           imageUrl="assets/images/guides/investors/buying-mue/step-2.png">
             <p className="guide-step__paragraph">
               <a className="guide-step__paragraph-link" href="http://www.bittylicious.com" target="_blank">Bittylicious</a> is a great option to quicky purchase MUE.</p>
             <p className="guide-step__paragraph">So head on over there, register an account, verify your email address and then log in and visit your profile.</p>
-            <p className="guide-step__paragraph">For higher transaction amounts you’ll need to enter your information to pass a few security checks. This will involve uploading identity documents. For more assistance, visit their helpful <a className="guide-step__paragraph-link" href="https://help.bittylicious.com/index.php/Main_Page" target="_blank">wiki</a>.</p>
+            <p className="guide-step__paragraph">For higher transaction amounts you'll need to enter your information to pass a few security checks. This will involve uploading identity documents. For more assistance, visit their helpful <a className="guide-step__paragraph-link" href="https://help.bittylicious.com/index.php/Main_Page" target="_blank">wiki</a>.</p>
         </GuideStep>
 
         <GuideStep
@@ -58,7 +58,7 @@ export default () => {
           tipOne=""
           tipTwo=""
           imageUrl="assets/images/guides/investors/buying-mue/step-1.png">
-            <p className="guide-step__paragraph">Your wallets address allows you to receive transactions from elsewhere and is what you’ll use to send Monetary Unit to yourself from Bittylicious.</p>
+            <p className="guide-step__paragraph">Your wallets address allows you to receive transactions from elsewhere and is what you'll use to send Monetary Unit to yourself from Bittylicious.</p>
             <p className="guide-step__paragraph">In the desktop wallet this can be found under:</p>
             <p className="guide-step__paragraph"><b>File > Receiving Addresses</b></p>
             <p className="guide-step__paragraph">Copy your wallet address <b>carefully!</b></p>
@@ -68,9 +68,9 @@ export default () => {
           step="4"
           title="Order your MUE"
           tipOne="Always double check your address has been copied correctly into Bittylicious!"
-          tipTwo="You’ll be notified of the progress of the transaction and should receive your payment within minutes."
+          tipTwo="You'll be notified of the progress of the transaction and should receive your payment within minutes."
           imageUrl="assets/images/guides/investors/buying-mue/step-4.png">
-            <p className="guide-step__paragraph">Go to the Bittylicious homepage and make you’re logged in and verified.</p>
+            <p className="guide-step__paragraph">Go to the Bittylicious homepage and make you're logged in and verified.</p>
             <p className="guide-step__paragraph">Select 'buy', enter your amount and most importantly ensure you choose Monetary Unit from the currency dropdown.</p>
             <p className="guide-step__paragraph">When ready, select 'get some coins' and enter your payment details. Job done!</p>
         </GuideStep>
