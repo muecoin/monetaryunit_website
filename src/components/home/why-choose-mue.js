2113902120bbf731c38ef6903c8a3c50381cfdb6
Mark Kneale <mark.kneale@antiblanks.com>
2017-07-20 11:13:41 +0100

    updated copy

--- src/components/home/why-choose-mue.js
+++ src/components/home/why-choose-mue.js
@@ -7,7 +7,7 @@ export default () => (
           <div className="why-choose-mue__intro-text-container">
             <h1 className="why-choose-mue__intro-heading">Why Choose MUE?</h1>
             <p className="why-choose-mue__intro-paragraph">Within the crypto currency industry MUE is a well established and respected brand with a mature ecosystem and progress is driven by a dedicated foundation team. The rate of growth since launch and the potential to continue this trend shows MUE is only heading in one direction.</p>
-            <p className="why-choose-mue__intro-paragraph">Recent migration to the Quark X11 wallet allows us to reach a larger market of miners and brings added security with a likely rise in price from a more advanced hash rate. The MasterNode network manages a pool of funds which is set aside to fund the project moving forward. Proposals put before MasterNodes allows for a democratic process over future decisions unlike other currencies in the market as well as an added layer of security over transactions on top of the miners.</p>
+            <p className="why-choose-mue__intro-paragraph">Recent migration to the Quark X11 wallet allows us to reach a larger market of miners and brings added security with a likely rise in price from a more advanced hash rate. The MasterNode network manages a pool of funds which is set aside to fund the project moving forward. Proposals put before MasterNodes allows for a democratic process over future decisions unlike other currencies in the market.</p>
             <h2 className="why-choose-mue__intro-sub-heading">In Progress Projects:</h2>
           </div>
         </div>
