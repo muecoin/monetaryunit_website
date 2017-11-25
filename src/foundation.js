4db7b6cb457da374378f3f69ed90af2707fba152
sotblad <sotiris2009@hotmail.com>
2017-11-25 21:23:28 +0200

    Create foundation.js

---
+++ src/foundation.js
@@ -0,0 +1,131 @@
+import React from 'react';
+
+export default ({title = "Foundation"}) => (
+  <div className="press-articles" id="press-articles">
+    <section className="press-articles__container">
+      
+
+      <div className="press-articles__articles-list">
+
+        <div className="press-articles__article-item">
+          <div className="press-articles__article">
+            <h1 className="press-articles__sub-heading">BizzyB</h1>
+            <a className="press-articles__visit-btn" href="https://twitter.com/BizzybMUE" target="_blank">Profile</a>
+            <b><p className="press-articles__description">Started in crypto:</p></b> July 2013
+<b><p className="press-articles__description">Current industry sector:</p></b> I.T
+<b><p className="press-articles__description">Previous industry experience:</p></b> Startups, Project management
+          </div>
+        </div>
+
+        <div className="press-articles__article-item">
+          <div className="press-articles__article">
+            <h1 className="press-articles__sub-heading">Pjcltd</h1>
+            <a className="press-articles__visit-btn" href="https://twitter.com/mphostinguk" target="_blank">Profile</a>
+            <b><p className="press-articles__description">Started in crypto:</p></b> June 2013
+<b><p className="press-articles__description">Current industry sector:</p></b> I.T. Hosting, SIP, 3CX
+<b><p className="press-articles__description">Previous industry experience:</p></b> -
+          </div>
+        </div>
+
+        <div className="press-articles__article-item">
+          <div className="press-articles__article">
+            <h1 className="press-articles__sub-heading">Bbobb</h1>
+            <a className="press-articles__visit-btn" href="INSERT SOCIAL MEDIA OR ANYTHING" target="_blank">Profile</a>
+            <b><p className="press-articles__description">Started in crypto:</p></b> October 2012
+<b><p className="press-articles__description">Current industry sector:</p></b> ICT
+<b><p className="press-articles__description">Previous industry experience:</p></b> Tech ops, PM, BDM
+          </div>
+        </div>
+
+        <div className="press-articles__article-item">
+          <div className="press-articles__article">
+            <h1 className="press-articles__sub-heading">Mike.N</h1>
+            <a className="press-articles__visit-btn" href="INSERT SOCIAL MEDIA OR ANYTHING" target="_blank">Profile</a>
+            <b><p className="press-articles__description">Started in crypto:</p></b> May 2013
+<b><p className="press-articles__description">Current industry sector:</p></b> Security
+<b><p className="press-articles__description">Previous industry experience:</p></b> -
+          </div>
+        </div>
+      </div>
+	
+	<h1 className="press-articles__heading"></h1>
+	
+      <div className="press-articles__articles-list">
+
+        <div className="press-articles__article-item">
+          <div className="press-articles__article">
+            <h1 className="press-articles__sub-heading">Roslinpl</h1>
+            <a className="press-articles__visit-btn" href="INSERT SOCIAL MEDIA OR ANYTHING" target="_blank">Profile</a>
+            <b><p className="press-articles__description">Started in crypto:</p></b> November 2013
+<b><p className="press-articles__description">Current industry sector:</p></b> I.T
+<b><p className="press-articles__description">Previous industry experience:</p></b> I.T
+          </div>
+        </div>
+
+        <div className="press-articles__article-item">
+          <div className="press-articles__article">
+            <h1 className="press-articles__sub-heading">Swtrse</h1>
+            <a className="press-articles__visit-btn" href="https://twitter.com/swtrse" target="_blank">Profile</a>
+            <b><p className="press-articles__description">Started in crypto:</p></b> September 2014
+<b><p className="press-articles__description">Current industry sector:</p></b> I.T
+<b><p className="press-articles__description">Previous industry experience:</p></b> -
+          </div>
+        </div>
+
+        <div className="press-articles__article-item">
+          <div className="press-articles__article">
+            <h1 className="press-articles__sub-heading">Theboccet</h1>
+            <a className="press-articles__visit-btn" href="https://twitter.com/theboccet" target="_blank">Profile</a>
+            <b><p className="press-articles__description">Started in crypto:</p></b> November 2013
+<b><p className="press-articles__description">Current industry sector:</p></b> Orange ISP Assistance
+<b><p className="press-articles__description">Previous industry experience:</p></b> Computer Hardware Maintenance Service ; Electronics Vendor.
+          </div>
+        </div>
+
+        <div className="press-articles__article-item">
+          <div className="press-articles__article">
+            <h1 className="press-articles__sub-heading">Sojuz88</h1>
+            <a className="press-articles__visit-btn" href="INSERT SOCIAL MEDIA OR ANYTHING" target="_blank">Profile</a>
+            <b><p className="press-articles__description">Started in crypto:</p></b> Autumn 2015
+<b><p className="press-articles__description">Current industry sector:</p></b> Web App Development
+<b><p className="press-articles__description">Previous industry experience:</p></b> -
+          </div>
+        </div>
+<h1 className="press-articles__heading"></h1>
+	
+      <div className="press-articles__articles-list">
+
+<div className="press-articles__article-item">
+          <div className="press-articles__article">
+            <h1 className="press-articles__sub-heading">C4</h1>
+            <a className="press-articles__visit-btn" href="https://twitter.com/c44c_" target="_blank">Profile</a>
+            <b><p className="press-articles__description">Started in crypto:</p></b> April-May 2013
+<b><p className="press-articles__description">Current industry sector:</p></b> Mechanical Engineering and Consulting
+<b><p className="press-articles__description">Previous industry experience:</p></b> Mech. Eng
+          </div>
+        </div>
+
+<div className="press-articles__article-item">
+          <div className="press-articles__article">
+            <h1 className="press-articles__sub-heading">Johans</h1>
+            <a className="press-articles__visit-btn" href="https://twitter.com/TheCryptoEmpire" target="_blank">Profile</a>
+            <b><p className="press-articles__description">Started in crypto:</p></b> January 2016
+<b><p className="press-articles__description">Current industry sector:</p></b> Teaching networking/IT skills / Crypto Miner
+<b><p className="press-articles__description">Previous industry experience:</p></b> -
+          </div>
+        </div>
+
+        <div className="press-articles__article-item">
+          <div className="press-articles__article">
+            <h1 className="press-articles__sub-heading">SotBlad</h1>
+            <a className="press-articles__visit-btn" href="https://twitter.com/sotblad" target="_blank">Profile</a>
+            <b><p className="press-articles__description">Started in crypto:</p></b> 5 May 2016
+<b><p className="press-articles__description">Current industry sector:</p></b> Security, Web App Development
+<b><p className="press-articles__description">Previous industry experience:</p></b> Web Hosting
+          </div>
+        </div>
+</div>
+      </div>
+    </section>
+  </div>
+)
