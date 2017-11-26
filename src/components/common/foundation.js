2e64f0c65ea681da4a8a268f266ccef03076e3fc
Sotiris Blad <sotiris2009@hotmail.com>
2017-11-26 22:33:35 +0200

    Updates

--- src/components/common/foundation.js
+++ src/components/common/foundation.js
@@ -10,7 +10,7 @@ export default ({title = "Foundation"}) => (
                 <div className="press-articles__article-item">
                     <div className="press-articles__article">
                         <h1 className="press-articles__sub-heading">BizzyB</h1>
-                        <a className="press-articles__visit-btn" href="https://twitter.com/BizzybMUE" target="_blank">Profile</a>
+                        <a className="press-articles__visit-btn" href="https://twitter.com/BizzybMUE" target="_blank">Twitter</a>    <a className="press-articles__visit-btn" href="https://bitcointalk.org/index.php?action=profile;u=237103" target="_blank">BitcoinTalk</a>    <a className="press-articles__visit-btn" href="https://www.linkedin.com/in/bizzy-b-0ab382150/" target="_blank">in</a>
                         <b><p className="press-articles__description">Started in crypto:</p></b> July 2013
                         <b><p className="press-articles__description">Current industry sector:</p></b> I.T
                         <b><p className="press-articles__description">Previous industry experience:</p></b> Startups,
@@ -22,7 +22,7 @@ export default ({title = "Foundation"}) => (
                     <div className="press-articles__article">
                         <h1 className="press-articles__sub-heading">Bbobb</h1>
                         <a className="press-articles__visit-btn" href="https://twitter.com/Cryptophi"
-                           onclick="return false;" target="_blank">Profile</a>
+                           onclick="return false;" target="_blank">Twitter</a>    <a className="press-articles__visit-btn" href="https://bitcointalk.org/index.php?action=profile;u=563625" target="_blank">BitcoinTalk</a>
                         <b><p className="press-articles__description">Started in crypto:</p></b> October 2012
                         <b><p className="press-articles__description">Current industry sector:</p></b> ICT
                         <b><p className="press-articles__description">Previous industry experience:</p></b> Tech ops,
@@ -33,7 +33,7 @@ export default ({title = "Foundation"}) => (
                 <div className="press-articles__article-item">
                     <div className="press-articles__article">
                         <h1 className="press-articles__sub-heading">C4</h1>
-                        <a className="press-articles__visit-btn" href="https://twitter.com/c44c_" target="_blank">Profile</a>
+                        <a className="press-articles__visit-btn" href="https://twitter.com/c44c_" target="_blank">Twitter</a>    <a className="press-articles__visit-btn" href="bcttalk" onclick="return false;" target="_blank">BitcoinTalk</a>
                         <b><p className="press-articles__description">Started in crypto:</p></b> April-May 2013
                         <b><p className="press-articles__description">Current industry sector:</p></b> Mechanical
                         Engineering and Consulting
@@ -45,7 +45,7 @@ export default ({title = "Foundation"}) => (
                     <div className="press-articles__article">
                         <h1 className="press-articles__sub-heading">Johans</h1>
                         <a className="press-articles__visit-btn" href="https://twitter.com/TheCryptoEmpire"
-                           target="_blank">Profile</a>
+                           target="_blank">Twitter</a>    <a className="press-articles__visit-btn" href="bcttalk" target="_blank">BitcoinTalk</a>
                         <b><p className="press-articles__description">Started in crypto:</p></b> January 2016
                         <b><p className="press-articles__description">Current industry sector:</p></b> Teaching
                         networking/IT skills / Crypto Miner
@@ -63,7 +63,7 @@ export default ({title = "Foundation"}) => (
                         <div className="press-articles__article">
                             <h1 className="press-articles__sub-heading">Mike.N</h1>
                             <a className="press-articles__visit-btn" href="#"
-                               onclick="return false;" target="_blank">Profile</a>
+                               onclick="return false;" target="_blank">Twitter</a>    <a className="press-articles__visit-btn" href="bcttalk" onclick="return false;" target="_blank">BitcoinTalk</a>
                             <b><p className="press-articles__description">Started in crypto:</p></b> May 2013
                             <b><p className="press-articles__description">Current industry sector:</p></b> Security
                             <b><p className="press-articles__description">Previous industry experience:</p></b> -
@@ -74,7 +74,7 @@ export default ({title = "Foundation"}) => (
                         <div className="press-articles__article">
                             <h1 className="press-articles__sub-heading">Pjcltd</h1>
                             <a className="press-articles__visit-btn" href="https://twitter.com/mphostinguk"
-                               target="_blank">Profile</a>
+                               target="_blank">Twitter</a>    <a className="press-articles__visit-btn" href="https://bitcointalk.org/index.php?action=profile;u=151913" target="_blank">BitcoinTalk</a>    <a className="press-articles__visit-btn" href="https://www.linkedin.com/in/paul-jenkinson-28482974" target="_blank">in</a>
                             <b><p className="press-articles__description">Started in crypto:</p></b> June 2013
                             <b><p className="press-articles__description">Current industry sector:</p></b> I.T. Hosting,
                             SIP, 3CX
@@ -86,7 +86,7 @@ export default ({title = "Foundation"}) => (
                         <div className="press-articles__article">
                             <h1 className="press-articles__sub-heading">Roslinpl</h1>
                             <a className="press-articles__visit-btn" href="https://twitter.com/Crypt0Hybr1d"
-                               onclick="return false;" target="_blank">Profile</a>
+                               onclick="return false;" target="_blank">Twitter</a>    <a className="press-articles__visit-btn" href="bcttalk" onclick="return false;" target="_blank">BitcoinTalk</a>
                             <b><p className="press-articles__description">Started in crypto:</p></b> November 2013
                             <b><p className="press-articles__description">Current industry sector:</p></b> I.T
                             <b><p className="press-articles__description">Previous industry experience:</p></b> I.T
@@ -95,13 +95,12 @@ export default ({title = "Foundation"}) => (
 
                     <div className="press-articles__article-item">
                         <div className="press-articles__article">
-                            <h1 className="press-articles__sub-heading">Sojuz88</h1>
-                            <a className="press-articles__visit-btn" href="#"
-                               onclick="return false;" target="_blank">Profile</a>
-                            <b><p className="press-articles__description">Started in crypto:</p></b> Autumn 2015
-                            <b><p className="press-articles__description">Current industry sector:</p></b> Web App
-                            Development
-                            <b><p className="press-articles__description">Previous industry experience:</p></b> -
+                            <h1 className="press-articles__sub-heading">NotSoFast</h1>
+                            <a className="press-articles__visit-btn" href="https://twitter.com/notsofast"
+                               onclick="return false;" target="_blank">Twitter</a>    <a className="press-articles__visit-btn" href="https://bitcointalk.org/index.php?action=profile;u=339189" target="_blank">BitcoinTalk</a>
+                            <b><p className="press-articles__description">Started in crypto:</p></b> 2014
+                            <b><p className="press-articles__description">Current industry sector:</p></b> Independent Advisor/Analyst
+                            <b><p className="press-articles__description">Previous industry experience:</p></b> Independent mining, Trading, and related research
                         </div>
                     </div>
                 </div>
@@ -111,10 +110,22 @@ export default ({title = "Foundation"}) => (
 
                 <div className="press-articles__articles-list">
 
+		<div className="press-articles__article-item">
+                        <div className="press-articles__article">
+                            <h1 className="press-articles__sub-heading">Sojuz88</h1>
+                            <a className="press-articles__visit-btn" href="#"
+                               onclick="return false;" target="_blank">Twitter</a>    <a className="press-articles__visit-btn" href="bcttalk" onclick="return false;" target="_blank">BitcoinTalk</a>
+                            <b><p className="press-articles__description">Started in crypto:</p></b> Autumn 2015
+                            <b><p className="press-articles__description">Current industry sector:</p></b> Web App
+                            Development
+                            <b><p className="press-articles__description">Previous industry experience:</p></b> -
+                        </div>
+                    </div>	
+
                     <div className="press-articles__article-item">
                         <div className="press-articles__article">
                             <h1 className="press-articles__sub-heading">SotBlad</h1>
-                            <a className="press-articles__visit-btn" href="https://twitter.com/sotblad" target="_blank">Profile</a>
+                            <a className="press-articles__visit-btn" href="https://twitter.com/sotblad" target="_blank">Twitter</a>    <a className="press-articles__visit-btn" href="https://bitcointalk.org/index.php?action=profile;u=883063" target="_blank">BitcoinTalk</a>
                             <b><p className="press-articles__description">Started in crypto:</p></b> 5 May 2016
                             <b><p className="press-articles__description">Current industry sector:</p></b> Security, Web
                             App Development
@@ -126,7 +137,7 @@ export default ({title = "Foundation"}) => (
                     <div className="press-articles__article-item">
                         <div className="press-articles__article">
                             <h1 className="press-articles__sub-heading">Swtrse</h1>
-                            <a className="press-articles__visit-btn" href="https://twitter.com/swtrse" target="_blank">Profile</a>
+                            <a className="press-articles__visit-btn" href="https://twitter.com/swtrse" target="_blank">Twitter</a>    <a className="press-articles__visit-btn" href="bcttalk" onclick="return false;" target="_blank">BitcoinTalk</a>
                             <b><p className="press-articles__description">Started in crypto:</p></b> September 2014
                             <b><p className="press-articles__description">Current industry sector:</p></b> I.T
                             <b><p className="press-articles__description">Previous industry experience:</p></b> -
@@ -137,7 +148,7 @@ export default ({title = "Foundation"}) => (
                         <div className="press-articles__article">
                             <h1 className="press-articles__sub-heading">Theboccet</h1>
                             <a className="press-articles__visit-btn" href="https://twitter.com/theboccet"
-                               target="_blank">Profile</a>
+                               target="_blank">Twitter</a>    <a className="press-articles__visit-btn" href="bcttalk" onclick="return false;" target="_blank">BitcoinTalk</a>
                             <b><p className="press-articles__description">Started in crypto:</p></b> November 2013
                             <b><p className="press-articles__description">Current industry sector:</p></b> ISP CS
                             <b><p className="press-articles__description">Previous industry experience:</p></b> Computer
