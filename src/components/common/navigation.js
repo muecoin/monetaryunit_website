c6e8b6d8b4958ee9eca7c64b2f84c09b9937e6dc
Sotiris Blad <sotiris2009@hotmail.com>
2017-11-27 19:16:04 +0200

    Update navigation.js

--- src/components/common/navigation.js
+++ src/components/common/navigation.js
@@ -30,10 +30,10 @@ export default ({ block = "nav", fixed = false }) => (
               <a className={`${block}__link`} href="/#join-the-community" data-jump-to=".join-the-community">Community</a>
             </li>
             <li className={`${block}__list-item`}>
-              <a className={`${block}__link`} href="/foundation.html" target="_blank">Foundation</a>
+              <a className={`${block}__link`} href="/foundation.html">Foundation</a>
             </li>
 	    <li className={`${block}__list-item`}>
-              <a className={`${block}__link`} href="/directors.html" target="_blank">Directors</a>
+              <a className={`${block}__link`} href="/directors.html">Directors</a>
             </li>
             <li className={`${block}__list-item`}>
               <a className={`${block}__link`} href="/frequently-asked-questions.html">FAQ</a>
