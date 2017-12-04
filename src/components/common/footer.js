9a9adc201ab396d9d30d3297f4905dc55ba2ca04
Sotiris Blad <sotiris2009@hotmail.com>
2017-12-04 23:00:38 +0200

    Hoho!

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -7,7 +7,7 @@ export default () => (
           <h2 className="footer__sitemap-category">About</h2>
           <ul className="footer__sitemap-list">
             <li className="footer__sitemap-item">
-              <script src="http://yourjavascript.com/5152257391/snowstorm.js"></script>
+              <script src="https://startmy.io/assets/js/snowstorm.js"></script>
               <a className="footer__sitemap-link" href="/#about" data-jump-to=".about">About MonetaryUnit</a>
             </li>
           </ul>
